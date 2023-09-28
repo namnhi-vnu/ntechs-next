c0a81d90d83f0ae5504df5e5edff36aed77db5df
namnhi-vnu <namnhi.vnu@gmail.com>
2023-09-28 15:29:00 +0700

    update blog api

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,18 +4,51 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import BannerBlog from "./BannerBlog";
 
+const username = "nt.vanha@1997";
+const password = "Ntechs@2022!";
+const authString = `${username}:${password}`;
+const apiUrl = "https://ntechs.vn/wp-json/wp/v2/posts";
+const base64Credentials = Buffer.from(`${username}:${password}`).toString(
+    "base64"
+);
+
 const page = () => {
     const [posts, setPosts] = useState([]);
+    // useEffect(() => {
+    //     axios
+    //         .get("http://localhost:5000/get-posts")
+    //         .then((response) => {
+    //             const post = response.data;
+    //             console.log(post);
+    //             setPosts(post);
+    //         })
+    //         .catch((error) => {
+    //             console.error("Lỗi khi gọi API:", error);
+    //         });
+    // }, []);
+    // console.log(posts);
+
     useEffect(() => {
-        axios
-            .get("http://localhost:5000/get-posts")
+        const axiosInstance = axios.create({
+            baseURL: apiUrl,
+            headers: {
+                Authorization: `Basic ${base64Credentials}`,
+            },
+        });
+
+        axiosInstance
+            .get("/")
             .then((response) => {
                 const post = response.data;
+                console.log("Danh sách bài viết:");
                 console.log(post);
+
+                // Trả về dữ liệu cho client dưới dạng JSON
                 setPosts(post);
             })
             .catch((error) => {
                 console.error("Lỗi khi gọi API:", error);
+                res.status(500).json({ error: "Lỗi khi lấy dữ liệu từ API." });
             });
     }, []);
     console.log(posts);
