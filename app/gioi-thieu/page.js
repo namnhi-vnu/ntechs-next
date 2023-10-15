245119beca6a0829a7c05e268cba9fb6ccb4d42b
namnhi-vnu <namnhi.vnu@gmail.com>
2023-10-15 21:43:03 +0700

    update gioi thieu

--- app/gioi-thieu/page.js
+++ app/gioi-thieu/page.js
@@ -6,12 +6,16 @@ const About = () => {
             <main className="">
                 <div className="banner-about text-center py-6 min-h-[350px] flex items-baseline justify-center">
                     <img src="./canvaleft.png" alt="" className="left" />
-                    <img src="./canvaleft.png" alt="" className="right" />
+                    <img
+                        src="./canvaleft.png"
+                        alt=""
+                        className="right max-md:hidden"
+                    />
                     <div className="z-20 relative">
-                        <h3 className="text-[1.6em] text-hover font-semibold pt-10">
+                        <h3 className="text-[1.6em] text-hover font-semibold pt-10 max-md:text-[1.275em]">
                             Chào mừng bạn đến với NTechs
                         </h3>
-                        <h2 className="text-[2.2em] text-ntechs font-semibold pt-2">
+                        <h2 className="text-[2.2em] text-ntechs font-semibold pt-2 max-md:text-[1.725em]">
                             Thiết kế và phát triển Web App, Mobile App đa nền
                             tảng
                         </h2>
@@ -19,7 +23,7 @@ const About = () => {
                 </div>
                 <div className=" bg-white about-contents relative">
                     <div className="container  mx-auto py-6 ">
-                        <div className="flex relative bg-white max-w-[850px] mt-[-8em] gap-4 rounded-lg mx-auto p-[1em] shadow-md">
+                        <div className="flex max-md:flex-col  relative bg-white max-w-[850px] mt-[-8em] gap-4 rounded-lg mx-auto p-[1em] shadow-md">
                             <img
                                 src="./canvaleft.png"
                                 alt=""
@@ -37,8 +41,8 @@ const About = () => {
                                     className="w-[35%] flex-1"
                                 />
                             </div>
-                            <div className="flex-1">
-                                <h2 className="text-[1.5em] text-ntechs font-medium">
+                            <div className="flex-1 max-md:order-first">
+                                <h2 className="text-[1.5em] text-ntechs font-medium z-30 relative">
                                     Về chúng tôi
                                 </h2>
                                 <p className="text-[#333] z-20 relative">
@@ -69,11 +73,61 @@ const About = () => {
                     </div>
                 </div>
                 <div className="">
-                    <div className="container mx-auto py-8">
+                    <div className="container mx-auto py-8 max-md:p-4">
                         <div>
                             <h2 className="text-[2em] text-ntechs text-center font-semibold">
                                 Mục tiêu sắp tới của NTechs
                             </h2>
+                        </div>
+                        <div className="mt-8">
+                            <div className="grid grid-cols-3 gap-8 max-md:grid-cols-1">
+                                <div className="p-4 rounded-lg hover:shadow-lg">
+                                    <div className="pb-8 flex items-center justify-center">
+                                        <img src="./medal-star.png" alt="" />
+                                    </div>
+                                    <div className="">
+                                        <h2 className="text-[1.4em] font-semibold text-center pb-3">
+                                            Trở thành đơn vị số 1
+                                        </h2>
+                                        <p className="text-lg">
+                                            Chuyên thiết kế phát triển ứng dụng
+                                            đa nền tảng, giúp các doanh nghiệp
+                                            phát triển nâng tầm thương hiệu
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="shadow-lg p-4 rounded-lg border border-hover">
+                                    <div className="pb-8 flex items-center justify-center">
+                                        <img src="./Icon-5.png" alt="" />
+                                    </div>
+                                    <div className="">
+                                        <h2 className="text-[1.4em] font-semibold text-center pb-3">
+                                            Hợp tác phát triển
+                                        </h2>
+                                        <p className="text-lg">
+                                            Cùng hơn 10.000 doanh nghiệp trong
+                                            quá trình xây dựng và phát triển
+                                            doanh nghiệp.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className=" p-4 rounded-lg hover:shadow-lg">
+                                    <div className="pb-8 flex items-center justify-center">
+                                        <img src="./Icon-2-1.png" alt="" />
+                                    </div>
+                                    <div className="">
+                                        <h2 className="text-[1.4em] font-semibold text-center pb-3">
+                                            Cung cấp mẫu thiết kế
+                                        </h2>
+                                        <p className="text-lg">
+                                            Tạo ra hơn 5000 mẫu thiết kế Mobile
+                                            App ưu việt giúp gia tăng lợi nhuận,
+                                            tối ưu chi phí và nâng cao giá trị
+                                            doanh nghiệp.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
